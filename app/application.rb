a606986638d455a5e10843cf6b073a49355bb4b3
Yomaira Escano <yjescano@gmail.com>
2018-01-11 02:53:24 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,11 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
       resp.write item.price
-    
+      if items.empty?
+        resp.write "Item not found"
+        resp.status = 400
+      end
+
     else
       resp.write "Route not found"
       resp.status = 404
