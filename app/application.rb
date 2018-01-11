b12fe7e3e6b47e21e9a16a69f40f116369561297
Yomaira Escano <yjescano@gmail.com>
2018-01-11 02:54:44 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name == item_name}
       resp.write item.price
-      if @@items.empty?
+      if item.empty?
         resp.write "Item not found"
         resp.status = 400
       end
