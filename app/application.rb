ab93a08bcdc1c3d32e23253eb31e0f320f439be2
Yomaira Escano <yjescano@gmail.com>
2018-01-11 01:51:09 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,13 @@
 class application
 
+  @@item = []
+
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    
+    if req.path == "/item"
+      resp.write ""
   end
 
 end
