e41e23301f6122b15df09b379307e591065a56a0
Yomaira Escano <yjescano@gmail.com>
2018-01-11 02:24:47 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,15 +9,15 @@ class Application
     @@items.each do |item|
       resp.write "#{item.price}\n"
 
-    @@items.each do |item|
-      @@items << Item.new(item.name, item.price)
-
-      if req.path.match == (/item/)
-        resp.write "#{item.price}"
-      else
-        resp.write "Route not found"
-        resp.status = 404
-      end
+    # @@items.each do |item|
+    #   @@items << Item.new(item.name, item.price)
+    #
+    #   if req.path.match == (/item/)
+    #     resp.write "#{item.price}"
+    #   else
+    #     resp.write "Route not found"
+    #     resp.status = 404
+    #   end
     end
 
     resp.finish
